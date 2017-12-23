5290e0e2f140252aa63e62edf9662e3e63c066a1
migaman <mikee@bluewin.ch>
2017-12-23 23:12:31 +0100

    process first nlp outcomes

--- index.js
+++ index.js
@@ -138,15 +138,15 @@ function getAnswer(nlpJson) {
 	var answer = "";
 	//Process JSON for correct answer
 	
-	if (nlpJson['entities'].['intent']['0']['value'] === 'temperature_get') {
+	if (nlpJson['entities']['intent']['0']['value'] === 'temperature_get') {
 		answer = "Die aktuelle Temperatur ist...";
-	}
-	else if (nlpJson['entities'].['intent']['0']['value'] === 'temperature_get') {
+	/*}
+	else if (nlpJson['entities']['intent']['0']['value'] === 'temperature_get') {
 		answer = "Die neue Temperatur ist " + nlpJson['entities']['temperature']['0']['value'] + ' ' + nlpJson['entities']['temperature']['0']['type'];
 	}
-	else if (nlpJson['entities'].['intent']['0']['value'] === 'restaurant') {
+	else if (nlpJson['entities']['intent']['0']['value'] === 'restaurant') {
 		answer = "Ich zeige dir eine Liste von Restaurants...";
-	}
+	}*/
 	else {
 		answer = "Ich verstehe deine Anfrage nicht. Sorry.";
 	}
