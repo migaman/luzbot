73737a967fc5c3b50254a3038cecd75e5debbcb1
migaman <mikee@bluewin.ch>
2017-12-25 16:59:24 +0100

    Example Frontend

--- index.js
+++ index.js
@@ -5,6 +5,26 @@ var bodyParser = require('body-parser');
 var request = require('request');  
 var app = express();
 
+// Optional. You will see this name in eg. 'ps' or 'top' command
+process.title = 'luzbot';
+
+// Port where we'll run the websocket server
+var webSocketsServerPort = 1337;
+
+// websocket and http servers
+var webSocketServer = require('websocket').server;
+var http = require('http');
+
+
+/**
+ * Global variables
+ */
+// latest 100 messages
+var history = [ ];
+// list of currently connected clients (users)
+var clients = [ ];
+
+
 let Wit = null;
 let log = null;
 try {
@@ -19,7 +39,7 @@ try {
 // Variables are defined in Heroku
 const WIT_TOKEN = process.env.WIT_TOKEN;
 const FB_PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 //Needs feature Dyno Metadata (https://stackoverflow.com/questions/7917523/how-do-i-access-the-current-heroku-release-version-programmatically)
 const VERSION = process.env.HEROKU_RELEASE_VERSION;
 
@@ -31,10 +51,45 @@ const pgClient = new Client({
 });
 
 
+/**
+ * Helper function for escaping input strings
+ */
+function htmlEntities(str) {
+    return String(str).replace(/&/g, '&amp;').replace(/</g, '&lt;')
+                      .replace(/>/g, '&gt;').replace(/"/g, '&quot;');
+}
+
+// Array with some colors
+var colors = [ 'green', 'blue', 'magenta', 'purple', 'plum', 'orange' ];
+// ... in random order
+colors.sort(function(a,b) { return Math.random() > 0.5; } );
+
+
 app.use(bodyParser.urlencoded({extended: false}));  
 app.use(bodyParser.json());  
 app.listen(PORT);
 console.log('Listening on :' + PORT + '...');
+
+
+/**
+ * HTTP server
+ */
+var server = http.createServer(function(request, response) {
+    // Not important for us. We're writing WebSocket server, not HTTP server
+});
+server.listen(webSocketsServerPort, function() {
+    console.log((new Date()) + " Server is listening on port " + webSocketsServerPort);
+});
+
+/**
+ * WebSocket server
+ */
+var wsServer = new webSocketServer({
+    // WebSocket server is tied to a HTTP server. WebSocket request is just
+    // an enhanced HTTP request. For more info http://tools.ietf.org/html/rfc6455#page-6
+    httpServer: server
+});
+
 
 // Server frontpage
 app.get('/', function (req, res) {  
@@ -68,144 +123,81 @@ app.post('/webhook', function (req, res) {
 		
 			var question = event.message.text;
 		
-		
-			
-			if(question.toUpperCase() == "HI") {
-				var answer = "Hi. How are you?";
-				sendMessage(event.sender.id, answer);
-			}
-			else if(question.toUpperCase() == "VERSION") {
-				var answer = "Aktuelle Version ist " + VERSION;
-				sendMessage(event.sender.id, answer);
-			}
-			else {
-				//forward question to wit framework
-				console.log('New message detected, text: ' + question);
-				console.log('New message detected, sender: ' + sender);
-				console.log('New message detected, sessionId: ' + sessionId);
-					
-				wit.message(question, {})
-					.then((data) => 
-					{
-						var body = JSON.stringify(data);
-						console.log('Yay, got Wit.ai response: ' + body);
-						var answer = getAnswer(data);
-						sendMessage(event.sender.id, answer);
-					})
-				.catch(console.error);
-				
-			}
+			console.log('New message detected, text: ' + question);
+			console.log('New message detected, sender: ' + sender);
 			
 			
-            
-			
-			
-		
-			// Let's forward the message to the Wit.ai Bot Engine
-            // This will run all actions until our bot has nothing left to do
-            /*wit.runActions(
-              sessionId, // the user's current session
-              text, // the user's message
-              sessions[sessionId].context // the user's current session state
-            ).then((context) => {
-              // Our bot did everything it has to do.
-              // Now it's waiting for further messages to proceed.
-              console.log('Waiting for next user messages');
-
-              // Based on the session state, you might want to reset the session.
-              // This depends heavily on the business logic of your bot.
-              // Example:
-              // if (context['done']) {
-              //   delete sessions[sessionId];
-              // }
-
-              // Updating the user's current session state
-              sessions[sessionId].context = context;
-            })
-            .catch((err) => {
-              console.error('Oops! Got an error from Wit: ', err.stack || err);
-            })
-			*/
-			
+			sendBotAnswer(2, sender, question);
 			
         }
     }
     res.sendStatus(200);
 });
 
-//witJSON answer of WIT Engine, i.e.
-/*
-{
-	"_text":"Zeige mir Restaurants"
-	,"entities":{"intent":[{"confidence":0.78031343410165,"value":"restaurant"}]}
-	,"msg_id":"0xaoNXhmAyWix0sms"
-}
-*/
-function getAnswer(nlpJson) {
-	var answer = "";
-	//Process JSON for correct answer
-
-	if (nlpJson['entities']['intent']['0']['value'] === 'temperature_get') {
-		answer = "Die aktuelle Temperatur ist...";
-	}
-	else if (nlpJson['entities']['intent']['0']['value'] === 'temperature_set') {
-		answer = "Die neue Temperatur ist " + nlpJson['entities']['temperature']['0']['value'] + ' ' + nlpJson['entities']['temperature']['0']['unit'];
-	}
-	else if (nlpJson['entities']['intent']['0']['value'] === 'restaurant') {
-		pgClient.connect();
-
-		answer = 'Ich zeige dir eine Liste von Restaurants...';
-		
-		var sql = "SELECT resultname FROM results res LEFT OUTER JOIN category cat ON res.idcategory = cat.idcategory LEFT OUTER JOIN subcategory scat ON res.idsubcategory = scat.idsubcategory WHERE category = 'Eating'";
-		
-		//'SELECT table_schema,table_name FROM information_schema.tables;'
-		
-		pgClient.query(sql, (err, res) => {
-		if (err) throw err;
-		for (let row of res.rows) {
-			console.log(JSON.stringify(row));
-			answer += JSON.stringify(row);
-			answer += "test";
-		}
-		pgClient.end();
-		});
-		
-		answer += "...Ende der Liste...";
-	}
-	else {
-		answer = "Ich verstehe deine Anfrage nicht. Sorry.";
-	}
-
-	/*
-	SELECT resultname FROM results res
-	LEFT OUTER JOIN category cat ON res.idcategory = cat.idcategory
-	LEFT OUTER JOIN subcategory scat ON res.idsubcategory = scat.idsubcategory
-	WHERE category = 'Eating'
-	*/
-	
-    return answer;
-}
 
 
-// generic function sending messages
-function sendMessage(recipientId, msg) {  
-	var message = {text: msg}; 
-    request({
-        url: 'https://graph.facebook.com/v2.10/me/messages',
-        qs: {access_token: FB_PAGE_ACCESS_TOKEN},
-        method: 'POST',
-        json: {
-            recipient: {id: recipientId},
-            message: message,
-        }
-    }, function(error, response, body) {
-        if (error) {
-            console.log('Error sending message: ', error);
-        } else if (response.body.error) {
-            console.log('Error: ', response.body.error);
+// This callback function is called every time someone tries to connect to the WebSocket server
+wsServer.on('request', function(request) {
+    console.log((new Date()) + ' Connection from origin ' + request.origin + '.');
+
+    // accept connection - you should check 'request.origin' to make sure that
+    // client is connecting from your website
+    // (http://en.wikipedia.org/wiki/Same_origin_policy)
+    var connection = request.accept(null, request.origin); 
+    // we need to know client index to remove them on 'close' event
+    var index = clients.push(connection) - 1;
+    var userName = false;
+    var userColor = false;
+
+    console.log((new Date()) + ' Connection accepted.');
+
+    // send back chat history
+    if (history.length > 0) {
+        connection.sendUTF(JSON.stringify( { type: 'history', data: history} ));
+    }
+
+    // user sent some message
+    connection.on('message', function(message) {
+        if (message.type === 'utf8') { // accept only text
+            if (userName === false) { // first message sent by user is their name
+                // remember user name
+                userName = htmlEntities(message.utf8Data);
+                // get random color and send it back to the user
+                userColor = colors.shift();
+                connection.sendUTF(JSON.stringify({ type:'color', data: userColor }));
+                console.log((new Date()) + ' User is known as: ' + userName
+                            + ' with ' + userColor + ' color.');
+
+            } else { // log and broadcast the message
+                console.log((new Date()) + ' Received Message from '
+                            + userName + ': ' + message.utf8Data);
+                
+				//Eingegeben Nachricht ausgeben...
+				sendMessageNativeBot(message.utf8Data, userName, userColor);
+				
+                //Sende Bot Anwort...
+				var question = message.utf8Data;
+				sendBotAnswer(1, 0, question);
+				
+
+            }
         }
     });
-};
+
+    // user disconnected
+    connection.on('close', function(connection) {
+        if (userName !== false && userColor !== false) {
+            console.log((new Date()) + " Peer "
+                + connection.remoteAddress + " disconnected.");
+            // remove user from the list of connected clients
+            clients.splice(index, 1);
+            // push back user's color to be reused by another user
+            colors.push(userColor);
+        }
+    });
+
+});
+
 
 
 
@@ -244,6 +236,7 @@ const fbMessage = (id, text) => {
 // sessionId -> {fbid: facebookUserId, context: sessionState}
 const sessions = {};
 
+
 const findOrCreateSession = (fbid) => {
   let sessionId;
   // Let's see if we already have a session for the user fbid
@@ -260,6 +253,8 @@ const findOrCreateSession = (fbid) => {
   }
   return sessionId;
 };
+
+
 
 // Our bot actions
 const actions = {
@@ -290,8 +285,7 @@ const actions = {
       return Promise.resolve()
     }
   },
-  // You should implement your custom actions here
-  // See https://wit.ai/docs/quickstart
+
 };
 
 
@@ -301,3 +295,176 @@ const wit = new Wit({
   actions,
   logger: new log.Logger(log.INFO)
 });
+
+
+
+
+//Process JSON for correct answer
+function sendAnswer(botType, recipientId, nlpJson) {
+	if(nlpJson.hasOwnProperty('entities') && Object.keys(nlpJson.entities).length > 0){
+		console.log('property entities exists and has at least one entity');
+	
+		if (nlpJson['entities']['intent']['0']['value'] === 'temperature_get') {
+			var answer = "Die aktuelle Temperatur ist...";
+			sendMessage(botType, recipientId, answer);
+		}
+		else if (nlpJson['entities']['intent']['0']['value'] === 'temperature_set') {
+			var answer = "Die neue Temperatur ist ";
+			if(nlpJson.entities.hasOwnProperty('temperature')) {
+				answer += nlpJson['entities']['temperature']['0']['value'] + ' ' + nlpJson['entities']['temperature']['0']['unit'];
+			}
+			else {
+				answer = "Ich verstehe die neue Temperatur nicht!";
+			}
+			
+			sendMessage(botType, recipientId, answer);
+		}
+		else if (nlpJson['entities']['intent']['0']['value'] === 'restaurant') {
+			var answer = 'Ich zeige dir eine Liste von Restaurants...';
+			
+			pgClient.connect();
+
+			var sql = "SELECT resultname FROM results res LEFT OUTER JOIN category cat ON res.idcategory = cat.idcategory LEFT OUTER JOIN subcategory scat ON res.idsubcategory = scat.idsubcategory WHERE category = 'Eating'";
+			
+			pgClient.query(sql, (err, res) => {
+				if (err) throw err;
+				for (let row of res.rows) {
+					answer += row.resultname + "; ";
+				}
+				answer += "...Ende der Liste...";
+				pgClient.end();
+			
+				sendMessage(botType, recipientId, answer);
+			
+			});
+			
+			
+		}
+	}
+	else {
+		var answer = "Ich verstehe deine Anfrage nicht. Sorry.";
+		sendMessage(botType, recipientId, answer);
+	}
+
+}
+
+
+
+
+function sendBotAnswer(botType, recipientId, question) {
+	
+	if(question.toUpperCase() == "HI") {
+		var answer = "Hi. How are you?";
+		sendMessage(botType, recipientId, answer);
+	}
+	else if(question.toUpperCase() == "VERSION") {
+		var answer = "Aktuelle Version ist " + VERSION;
+		sendMessage(botType, recipientId, answer);
+	}
+	else {
+		//forward question to wit framework
+				
+		wit.message(question, {})
+			.then((data) => 
+			{
+				var body = JSON.stringify(data);
+				console.log('Wit.ai response: ' + body);
+				sendAnswer(botType, recipientId, data);
+			})
+		.catch(console.error);
+	}
+}
+
+function sendMessage(botType, recipientId, msg) {
+	if(botType == 1) {
+		//Native Chat
+		sendMessageNativeBot(msg, 'Luzbot', 'red');
+	}
+	else if(botType == 2) {
+		//Facebook Chat
+		sendMessageFacebook(recipientId, msg);
+	}
+}
+
+
+function sendMessageNativeBot(msg, userName, userColor) {
+	// we want to keep history of all sent messages
+	var obj = {
+		time: (new Date()).getTime(),
+		text: htmlEntities(msg),
+		author: userName,
+		color: userColor
+	};
+	history.push(obj);
+	history = history.slice(-100);
+
+	// broadcast message to all connected clients
+	var json = JSON.stringify({ type:'message', data: obj });
+	for (var i=0; i < clients.length; i++) {
+		clients[i].sendUTF(json);
+	}
+
+}
+
+
+
+function getAnswer(nlpJson) {
+	var answer = "";
+	//Process JSON for correct answer
+
+	if (nlpJson['entities']['intent']['0']['value'] === 'temperature_get') {
+		answer = "Die aktuelle Temperatur ist...";
+	}
+	else if (nlpJson['entities']['intent']['0']['value'] === 'temperature_set') {
+		answer = "Die neue Temperatur ist " + nlpJson['entities']['temperature']['0']['value'] + ' ' + nlpJson['entities']['temperature']['0']['unit'];
+	}
+	else if (nlpJson['entities']['intent']['0']['value'] === 'restaurant') {
+		pgClient.connect();
+
+		answer = 'Ich zeige dir eine Liste von Restaurants...';
+		
+		var sql = "SELECT resultname FROM results res LEFT OUTER JOIN category cat ON res.idcategory = cat.idcategory LEFT OUTER JOIN subcategory scat ON res.idsubcategory = scat.idsubcategory WHERE category = 'Eating'";
+		
+		//'SELECT table_schema,table_name FROM information_schema.tables;'
+	
+	
+		pgClient.query(sql, (err, res) => {
+		if (err) throw err;
+		for (let row of res.rows) {
+			console.log(JSON.stringify(row));
+			answer += JSON.stringify(row);
+		}
+		pgClient.end();
+		});
+		
+		answer += "...Ende der Liste...";
+	}
+	else {
+		answer = "Ich verstehe deine Anfrage nicht. Sorry.";
+	}
+
+    return answer;
+}
+
+// ----------------------------------------------------------------------------
+// Facebook Messenger specific code
+
+//Sends a Message in Facebook Chat
+function sendMessageFacebook(recipientId, msg) {  
+	var message = {text: msg}; 
+    request({
+        url: 'https://graph.facebook.com/v2.10/me/messages',
+        qs: {access_token: FB_PAGE_ACCESS_TOKEN},
+        method: 'POST',
+        json: {
+            recipient: {id: recipientId},
+            message: message,
+        }
+    }, function(error, response, body) {
+        if (error) {
+            console.log('Error sending message: ', error);
+        } else if (response.body.error) {
+            console.log('Error: ', response.body.error);
+        }
+    });
+};
